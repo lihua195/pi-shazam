e22c309e57599478483d21b447f0cdd67b762644
gjczone <361713969@qq.com>
2026-06-05 15:20:08 +0800

    chore: initial project scaffold

---
+++ index.ts
@@ -0,0 +1,5 @@
+import type { ExtensionAPI } from "@oh-my-pi/pi-coding-agent";
+
+export default function(pi: ExtensionAPI): void {
+  // placeholder - tools, hooks, commands will be registered here
+}
